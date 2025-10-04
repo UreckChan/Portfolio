605461d2d92f5f6b8ce77161e985a13758d65878
UreckChan <urielgomezbecerril@gmail.com>
2025-10-04 13:54:51 -0600

    Little changes

--- src/components/main/Me.jsx
+++ src/components/main/Me.jsx
@@ -8,7 +8,9 @@ const {lenguajeData,lenguaje} = useLenguaje();
     <section id="me" className="me-section settings-section">
         <h1 className="me-title">{lenguajeData["name"]}</h1>
         <p className="me-description">{lenguajeData["carrera-title"][lenguaje]}</p>
+        <a href="#projects" style={{ textDecoration: 'none' }}>
         <button className="button-projects"><strong>{lenguajeData["button-projects"][lenguaje]}</strong></button>
+        </a>
     </section>
   );
 };
