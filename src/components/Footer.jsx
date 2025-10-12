79bfa865a736fab03bf17cbe110ffdda17894238
UreckChan <urielgomezbecerril@gmail.com>
2025-10-12 01:26:15 -0600

    Little Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,15 @@ const {lenguajeData,lenguaje} = useLenguaje();
   return (
     <footer id="contact" className="footer">
       <p>{lenguajeData["footer-title"][lenguaje]}</p>
+
       <a href={`mailto:${lenguajeData["mail"]}`}><strong>{lenguajeData["mail"]}</strong></a>
+
+  <div class="social-icons">
+    <a href="https://github.com/UreckChan/" target="_blank" class="icon github"></a>
+    <a href="https://www.linkedin.com/in/uriel-gomez-becerril" target="_blank" class="icon linkedin"></a>
+    <a href="https://ureckchan.github.io/Portfolio/" target="_blank" class="icon portfolio"></a>
+  </div>
+
       <small> &copy; {year} {lenguajeData["footer-copyright"][lenguaje]}</small>
     </footer>
   );
