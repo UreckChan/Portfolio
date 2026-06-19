47bd7c09fbcc7e709062fcd75dbdacbe460fc96c
Uriel Gómez <106498831+UreckChan@users.noreply.github.com>
2026-06-18 19:52:55 -0600

    Cambios

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://ureckchan.github.io/Portfolio', // Configura la base para despliegue en subdirectorio
-  publicDir: 'public'
+  base: '/Portfolio/',
+  publicDir: 'public',
+  server: {
+    https: {
+      key: fs.readFileSync('.certs/localhost-key.pem'),
+      cert: fs.readFileSync('.certs/localhost.pem'),
+    },
+  },
 })
